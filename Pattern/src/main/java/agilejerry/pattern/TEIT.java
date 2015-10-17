750cf952ae6d3c23eef6787ae9f9d444bf7bb46f
zhangkeqiang <zhangkeqiang@tsjinrong.cn>
2015-10-17 20:06:06 +0800

    remove some todo

--- Pattern/src/main/java/agilejerry/pattern/TEIT.java
+++ Pattern/src/main/java/agilejerry/pattern/TEIT.java
@@ -4,13 +4,11 @@ public class TEIT extends EEIT {
 
 	@Override
 	protected int run1() {
-		// TODO Auto-generated method stub
 		return 2;
 	}
 
 	@Override
 	protected int run2() {
-		// TODO Auto-generated method stub
 		return 3;
 	}
     
