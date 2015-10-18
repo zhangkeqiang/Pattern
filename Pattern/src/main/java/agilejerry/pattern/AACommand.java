95f87ddc12b837e0d614ea4069e97e1d208b19a3
zhangkeqiang <zhangkeqiang@tsjinrong.cn>
2015-10-18 20:54:39 +0800

    fffffffffff

--- Pattern/src/main/java/agilejerry/pattern/AACommand.java
+++ Pattern/src/main/java/agilejerry/pattern/AACommand.java
@@ -8,13 +8,14 @@ public class AACommand implements Command {
 	}
 	
 	public AACommand(){
-		//add some
+		//add some fffffffffffff
 		//dddd ffffffff
 		this.II = 10;
 	}
 	public boolean execute() {
 		System.out.println("AACommand"  + II);
 		//change some in dev 
+		//ffffffffffff
 		return true;
 	}
 
