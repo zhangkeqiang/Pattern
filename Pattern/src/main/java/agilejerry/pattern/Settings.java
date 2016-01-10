557fb9101b6777b839b3c08f3bef85b7e01ac3bd
zhangmike <zhangmike@sina.com>
2016-01-10 19:41:08 +0800

    Change some thing

--- Pattern/src/main/java/agilejerry/pattern/Settings.java
+++ Pattern/src/main/java/agilejerry/pattern/Settings.java
@@ -2,7 +2,7 @@ package agilejerry.pattern;
 
 public class Settings {
 
-	public static String PRODUCT = "AA";
+	public static final String PRODUCT = "AA";
 
 	public static String getProduct() {
 		return PRODUCT;
